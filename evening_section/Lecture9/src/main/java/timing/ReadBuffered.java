1fe2ed23b294bc451cb995d7a418d18712fc5811
Abi Evans <ab.evans@northeastern.edu>
2020-03-07 15:52:47 -0800

    lecture 9 evening

---
+++ evening_section/Lecture9/src/main/java/timing/ReadBuffered.java
@@ -0,0 +1,23 @@
+package timing;
+
+import java.io.*;
+
+/**
+ * More efficient Java I/O approach. Uses a buffer to read by line.
+ */
+public class ReadBuffered extends AbstractRead {
+    @Override
+    protected void read(String filePath) {
+        try (BufferedReader inputFile =new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+            while ((line = inputFile.readLine()) != null) {
+                // do nothing
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("*** OOPS! A file was not found : " + fnfe.getMessage());
+        } catch (IOException ioe) {
+            System.out.println("Something went wrong! : " + ioe.getMessage());
+        }
+    }
+}
