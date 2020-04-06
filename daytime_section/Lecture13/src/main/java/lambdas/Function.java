396709308cef6de921361115323ab8e539c627cd
tbonaci <t.bonaci@northeastern.edu>
2020-04-06 13:42:45 -0700

    Lecture 13
    Lecture 13

---
+++ daytime_section/Lecture13/src/main/java/lambdas/Function.java
@@ -0,0 +1,16 @@
+package lambdas;
+
+/**
+ * Created by therapon on 7/6/16.
+ */
+public interface Function <I,O> {
+
+    /**
+     * Apply this funtction to {@code input} and return the result.
+     *
+     * @param input fucntion's input
+     * @return return the result of applying this function to input.
+     */
+    O apply(I input);
+
+}
